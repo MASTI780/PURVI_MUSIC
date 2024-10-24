fbedbf59c3205ae7fe800226635c76b691b9e543
GARIB-CODERS <landrepo541@gmail.com>
2024-10-24 14:13:00 +0530

    Update play.py

--- SONALI/utils/inline/play.py
+++ SONALI/utils/inline/play.py
@@ -34,40 +34,7 @@ def track_markup(_, videoid, user_id, channel, fplay):
     return buttons
 
 
-def stream_markup_timer(_, videoid, chat_id, played, dur):
-    played_sec = time_to_seconds(played)
-    duration_sec = time_to_seconds(dur)
-    percentage = (played_sec / duration_sec) * 100
-    umm = math.floor(percentage)
-    if 0 < umm <= 10:
-        bar = "▰▱▱▱▱▱▱▱▱▱"
-    elif 10 < umm < 20:
-        bar = "▰▰▱▱▱▱▱▱▱▱"
-    elif 20 <= umm < 30:
-        bar = "▰▰▰▱▱▱▱▱▱▱"
-    elif 30 <= umm < 40:
-        bar = "▰▰▰▰▱▱▱▱▱▱"
-    elif 40 <= umm < 50:
-        bar = "▰▰▰▰▰▱▱▱▱▱"
-    elif 50 <= umm < 60:
-        bar = "▰▰▰▰▰▰▱▱▱▱"
-    elif 60 <= umm < 70:
-        bar = "▰▰▰▰▰▰▰▱▱▱"
-    elif 70 <= umm < 80:
-        bar = "▰▰▰▰▰▰▰▰▱▱"
-    elif 80 <= umm < 95:
-        bar = "▰▰▰▰▰▰▰▰▰▱"
-    else:
-        bar = "▰▰▰▰▰▰▰▰▰▰"
-        
     buttons = [
-        [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
-        ],
         [
             InlineKeyboardButton(text="˹ ᴘɾꪮɱꪮᴛιꪮɳ ᴀʋᴀιʅᴀʙʅҽ ˼", callback_data=f"promotext"),
         ],
@@ -76,10 +43,10 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
         ],
         [
             InlineKeyboardButton(
-                text="˹ ᴜᴘᴅᴀᴛҽ ˼", url="https://t.me/BABY09_WORLD",
+                text="˹ ᴜᴘᴅᴀᴛҽ ˼", url="https://t.me/PURVI_SUPPORT",
             ),
             InlineKeyboardButton(
-                text="˹ 𝛅ᴜᴘᴘᴏꝛᴛ ˼", url="https://t.me/+OL6jdTL7JAJjYzVl",
+                text="˹ 𝛅ᴜᴘᴘᴏꝛᴛ ˼", url="https://t.me/+9ZDy2Q5zdGgwMGNl",
             )
         ],
         [
@@ -100,10 +67,10 @@ def stream_markup(_, videoid, chat_id):
         ],
         [
             InlineKeyboardButton(
-                text="˹ ᴜᴘᴅᴀᴛҽ ˼", url="https://t.me/BABY09_WORLD",
+                text="˹ ᴜᴘᴅᴀᴛҽ ˼", url="https://t.me/PURVI_SUPPORT",
             ),
             InlineKeyboardButton(
-                text="• 𝛅ᴜᴘᴘᴏꝛᴛ •", url="https://t.me/+OL6jdTL7JAJjYzVl",
+                text="• 𝛅ᴜᴘᴘᴏꝛᴛ •", url="https://t.me/+9ZDy2Q5zdGgwMGNl",
             )
         ],
         [
