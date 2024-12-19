8881891995c9155473f585979348d6ed0018af7b
MASTI780 <gunjansingh97093@gmail.com>
2024-12-19 09:16:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -40,7 +40,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/GARIB-CODERS/PURVI_MUSIC",
+    "https://github.com/MASTI780/PURVI_MUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
